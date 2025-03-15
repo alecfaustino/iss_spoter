c12f4beca89209d051423abd3db0b3b2a5bd52f4
Alec Faustino <alecfaustino@outlook.com>
2025-03-15 11:56:12 -0600

    implement fetchCordsByIP function

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const {fetchMyIP} = require('./iss');
+const {fetchCordsByIP} = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log('There was an error: ', error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log('There was an error: ', error);
+//     return;
+//   }
     
-  console.log("Successfully gathered IP: ", ip);
-});
+//   console.log("Successfully gathered IP: ", ip);
+// });
+
+// fetchCordsByIP("42", (error, data) => {
+//   if (error) {
+//     console.log(error.message);
+//     return;
+//   }
+
+//   console.log("Successfully found coordinates: ", data);
+// });
--- iss.js
+++ iss.js
@@ -15,6 +15,28 @@ const fetchMyIP = (callback) => {
   });
 };
 
+const fetchCordsByIP = (ip, callback) => {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) return callback(error, null);
+
+    // const parsedResponse = JSON.parse(response);
+    if (!body.success) {
+      const msg = `It did not work! Error: Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = {
+      latitude: body.latitude,
+      longitude: body.longitude,
+    };
+    callback(null, data);
+  });
+};
+
+
 module.exports = {
   fetchMyIP,
+  fetchCordsByIP,
 };
