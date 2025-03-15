be146c9d7a6419c8c2e13608df1d0ead53aaa7ac
Alec Faustino <alecfaustino@outlook.com>
2025-03-15 10:41:54 -0600

    implement fetchMyIP function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const {fetchMyIP} = require('./iss');
 
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log('There was an error: ', error);
+//     return;
+//   } else {
+//     console.log("Successfully gathered IP: ", ip);
+//   }
+// });
--- iss.js
+++ iss.js
@@ -1,1 +1,22 @@
+const needle = require('needle');
 
+const fetchMyIP = (callback) => {
+  needle.get('https://api.ipify.org?format=json', (error, response) => {
+    const strJSON = JSON.stringify(response.body);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.StatusCode} when fetching IP. Response: ${response.body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    callback(null, strJSON);
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+};
